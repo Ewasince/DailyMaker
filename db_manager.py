2259e6738ef7dd7ff383c392b3db2a46811fe37e
Ewasince <ewasince@gmail.com>
2022-06-05 22:53:43 +0300

    added hello event (beta)

--- db_manager.py
+++ db_manager.py
@@ -301,134 +301,7 @@ class Load_manager:
                 flag_connection = True
             cursor = connection.cursor()
 
-            query_repeat_models = f'''SELECT * FROM repeat_model'''
-            cursor.execute(query_repeat_models)
-            repeat_models_raw = cursor.fetchall()
-            for i in repeat_models_raw:  # в этом цикле перебираются все rm_model и создаются для них
-                # event'ы, оторые попадают в просматривамый промежуток
-                rm_id = i[0]  # rm расшифровывается как repeat_model
-                rm_type = i[1]
-                query_events_rm = f'''SELECT id FROM events WHERE rm_id = {rm_id}'''
-                cursor.execute(query_events_rm)
-                events_rm_raw = cursor.fetchall()
-                if len(events_rm_raw) == 0:  # если у перебираемого repeat_model нету event'ов, то он удаляется
-                    delete_event(cursor, rm_type, rm_id)
-                    continue
-                query_events_rm_int = list(map(lambda x: str(x[0]), events_rm_raw))
-                query_events_rm_str = '(' + ', '.join(query_events_rm_int) + ')'
-
-                query_last_entry_date = f'''SELECT * from date WHERE id IN {query_events_rm_str} AND
-                date = (SELECT MAX(date) FROM date WHERE id IN {query_events_rm_str})'''
-
-                cursor.execute(query_last_entry_date)
-                last_entry_date = cursor.fetchall()[0]
-
-                query_last_entry_event = f'''SELECT * FROM events WHERE id = {last_entry_date[0]}'''
-                cursor.execute(query_last_entry_event)
-                last_entry_event = cursor.fetchall()[0]
-
-                query_last_entry_tfrom = f'''SELECT * FROM time_from WHERE id = {last_entry_date[0]}'''
-                cursor.execute(query_last_entry_tfrom)
-                last_entry_time_from = cursor.fetchall()[0]
-
-                query_last_entry_tto = f'''SELECT * FROM time_to WHERE id = {last_entry_date[0]}'''
-                cursor.execute(query_last_entry_tto)
-                last_entry_time_to = cursor.fetchall()[0]
-
-                query_last_entry_tags = f'''SELECT * FROM tags WHERE id = {last_entry_date[0]}'''
-                cursor.execute(query_last_entry_tags)
-                last_entry_tags = list(map(lambda x: x[1], cursor.fetchall()))
-
-                e_event = plan_event()
-                e_event.name = last_entry_event[1]
-                e_event.description = last_entry_event[2]
-                e_event.time_from = convert_to_QTime(cursor, last_entry_time_from[1])
-                e_event.time_to = convert_to_QTime(cursor, last_entry_time_to[1])
-
-                e_event.date = convert_to_QDate(cursor, last_entry_date[1])
-                e_event.tags = last_entry_tags
-
-                pass
-                rm_interval = i[2]
-                match rm_type:
-                    case Gaps.day.value:
-                        next_date = QDate(e_event.date.year(),
-                                          e_event.date.month(),
-                                          e_event.date.day())
-                        next_date = next_date.addDays(rm_interval)
-                        while date_to > next_date:
-                            if exist_event(cursor, next_date, rm_id):
-                                continue
-
-                            e_event.date = next_date
-                            create_event(cursor, e_event, rm_id)
-                            next_date = next_date.addDays(rm_interval)
-                    case Gaps.week.value:
-                        next_date = QDate(e_event.date.year(),
-                                          e_event.date.month(),
-                                          e_event.date.day())
-                        # next_date = next_date.addDays(rm_interval * 7)
-                        next_date = e_event.date.addDays((-1) * (next_date.dayOfWeek() - 1))
-                        while date_to.toJulianDay() + 7 >= next_date.toJulianDay():
-                            query_days = f'''SELECT day FROM repeat_model_days WHERE id = {rm_id}'''
-                            cursor.execute(query_days)
-                            days = list(map(lambda x: x[0], cursor.fetchall()))
-
-                            for j in range(7):
-                                next_date_ = next_date.addDays(j)
-                                try:
-                                    if next_date_.toJulianDay() <= e_event.date.toJulianDay():
-                                        continue
-                                    if exist_event(cursor, next_date_, rm_id):
-                                        continue
-                                    days.index(next_date_.dayOfWeek() - 1)
-
-                                    e_event.date = next_date_
-                                    create_event(cursor, e_event, rm_id)
-                                    connection.commit()
-                                except Exception as e:
-                                    pass
-                            next_date = next_date.addDays(rm_interval * 7)
-                    case Gaps.month.value:
-                        next_date = QDate(e_event.date.year(), e_event.date.month(), 1)
-                        # next_date = next_date.addMonths(rm_interval)
-                        while date_to.toJulianDay() + 31 >= next_date.toJulianDay():
-                            query_days = f'''SELECT day FROM repeat_model_days WHERE id = {rm_id}'''
-                            cursor.execute(query_days)
-                            days = list(map(lambda x: x[0], cursor.fetchall()))
-
-                            next_date_ = QDate(next_date.year(), next_date.month(), next_date.day())
-                            for j in range(31):
-                                next_date_ = next_date_.addDays(1)
-                                try:
-                                    if next_date_ <= next_date:
-                                        continue
-                                    if exist_event(cursor, next_date_, rm_id):
-                                        continue
-                                    days.index(next_date_.day() - 1)
-
-                                    e_event.date = next_date_
-                                    create_event(cursor, e_event, rm_id)
-                                    connection.commit()
-                                except Exception as e:
-                                    test = e
-                                    pass
-                            next_date = next_date.addMonths(rm_interval)
-                    case Gaps.year.value:
-                        next_date = QDate(e_event.date.year(),
-                                          e_event.date.month(),
-                                          e_event.date.day())
-                        next_date = next_date.addYears(rm_interval)
-                        while date_to > next_date:
-                            query_year = f'''SELECT day, month FROM repeat_model_year WHERE id = {rm_id}'''
-                            cursor.execute(query_year)
-                            year_days = cursor.fetchall()[0]
-                            next_date_ = QDate(next_date.year(), year_days[1], year_days[0])
-
-                            e_event.date = next_date_
-                            create_event(cursor, e_event, rm_id)
-                            next_date = next_date.addYears(rm_interval)
-                connection.commit()
+            self.complete_repeat_events()
 
             # Список со всеми id событий за определенный промежуток
             event_ids = self.find_events(date_from, date_to, connection)
@@ -446,6 +319,8 @@ class Load_manager:
         finally:
             if flag_connection and connection:
                 connection.close()
+
+
 
     # Метод который получает на вход id события и выводить полную информацию по данному событию
     def find_event_by_id(self, id, cursor) -> plan_event:
